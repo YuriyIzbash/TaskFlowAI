bfc64d3041f083e05f2bc07b8a7b56b644daca77
Yuriy Izbash <izbash91@gmail.com>
2025-12-15 14:10:08 +0100

    Initial Commit

---
+++ TaskFlowAI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskFlowAI
+//
+//  Created by yuriy on 15. 12. 25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskFlowAI/TaskFlowAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowAIApp.swift
+//  TaskFlowAI
+//
+//  Created by yuriy on 15. 12. 25.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
