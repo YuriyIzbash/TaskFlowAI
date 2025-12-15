5cc9b1166dba8b7b5d7febe8775eb21e9888e9be
Yuriy Izbash <izbash91@gmail.com>
2025-12-15 16:55:19 +0100

    Updated TaskFlowAIApp

--- TaskFlowAI/TaskFlowAIApp.swift
+++ TaskFlowAI/TaskFlowAIApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct TaskFlowAIApp: App {
+    private let viewModel: AgentViewModel = {
+        let client = LLMAPIClient(
+            baseURL: URL(string: "http://localhost:1234/v1/chat/completions")!,
+            model: "meta-llama-3.1-8b-instruct",
+            apiKey: nil
+        )
+        let planner = AgentPlanner(client: client)
+        let executor = ToolExecutor()
+        return AgentViewModel(planner: planner, executor: executor)
+    }()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewModel: viewModel)
         }
     }
 }
